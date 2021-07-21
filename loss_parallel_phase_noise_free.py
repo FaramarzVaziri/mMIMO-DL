21e13477e94a2a4b1b6e33eee8da065c83f918a4
fara <fara@faraland.com>
2021-07-21 13:55:35 -0700

    sss

--- loss_parallel_phase_noise_free.py
+++ loss_parallel_phase_noise_free.py
@@ -43,25 +43,25 @@ class loss_parallel_phase_noise_free_class:
 
     @tf.function
     def C_per_sample(self,bundeled_inputs):
-        # # impl 1-----------------------------------------------------------------
-        # V_D_cplx, W_D_cplx, H_complex, V_RF_cplx, W_RF_cplx = bundeled_inputs
-        #
-        # # some pre-processeing on inputs
-        # V_RF_cplx_vectorized = tf.tile([V_RF_cplx], multiples=[self.K, 1, 1])
-        # W_RF_cplx_vectorized = tf.tile([W_RF_cplx], multiples=[self.K, 1, 1])
-        # bundeled_inputs_vectorized_on_k = [V_D_cplx, W_D_cplx, H_complex, V_RF_cplx_vectorized,
-        #                                    W_RF_cplx_vectorized]  # vectorized on k
-        # T0 = tf.map_fn(self.C_per_sample_per_k, bundeled_inputs_vectorized_on_k,
-        #                fn_output_signature=tf.float32, parallel_iterations=self.K) #
-        # return tf.reduce_mean(T0)
-
-
-        # impl 2 -----------------------------------------------------------------
+        # impl 1-----------------------------------------------------------------
         V_D_cplx, W_D_cplx, H_complex, V_RF_cplx, W_RF_cplx = bundeled_inputs
-        T0 = 0
-        for k in range(self.K):
-            T0 = T0 + self.C_per_sample_per_k([V_D_cplx[k,:], W_D_cplx[k,:], H_complex[k,:], V_RF_cplx, W_RF_cplx])
-        return T0/self.K
+
+        # some pre-processeing on inputs
+        V_RF_cplx_vectorized = tf.tile([V_RF_cplx], multiples=[self.K, 1, 1])
+        W_RF_cplx_vectorized = tf.tile([W_RF_cplx], multiples=[self.K, 1, 1])
+        bundeled_inputs_vectorized_on_k = [V_D_cplx, W_D_cplx, H_complex, V_RF_cplx_vectorized,
+                                           W_RF_cplx_vectorized]  # vectorized on k
+        T0 = tf.map_fn(self.C_per_sample_per_k, bundeled_inputs_vectorized_on_k,
+                       fn_output_signature=tf.float32, parallel_iterations=self.K) #
+        return tf.reduce_mean(T0)
+
+
+        # # impl 2 -----------------------------------------------------------------
+        # V_D_cplx, W_D_cplx, H_complex, V_RF_cplx, W_RF_cplx = bundeled_inputs
+        # T0 = 0
+        # for k in range(self.K):
+        #     T0 = T0 + self.C_per_sample_per_k([V_D_cplx[k,:], W_D_cplx[k,:], H_complex[k,:], V_RF_cplx, W_RF_cplx])
+        # return T0/self.K
 
 
 
@@ -105,22 +105,22 @@ class loss_parallel_phase_noise_free_class:
     @tf.function
     @tf.autograph.experimental.do_not_convert
     def ergodic_capacity(self, bundeled_inputs):
-        # # impl 1
-        # V_D_cplx, W_D_cplx, H, V_RF_cplx, W_RF_cplx = bundeled_inputs
-        # H_complex = tf.complex(H[:, :, :, :, 0], H[:, :, :, :, 1])
-        # bundeled_inputs_modified = [V_D_cplx, W_D_cplx, H_complex, V_RF_cplx, W_RF_cplx]
-        # T0 = tf.map_fn(self.C_per_sample, bundeled_inputs_modified, fn_output_signature=tf.float32, parallel_iterations=self.BATCHSIZE) #
-        # return tf.multiply(-1.0, tf.reduce_mean(T0))
-
-
-        # iml 2
+        # impl 1
         V_D_cplx, W_D_cplx, H, V_RF_cplx, W_RF_cplx = bundeled_inputs
         H_complex = tf.complex(H[:, :, :, :, 0], H[:, :, :, :, 1])
-        T = 0
-        for ij in range(self.BATCHSIZE):
-            T = T + self.C_per_sample( [V_D_cplx[ij,:], W_D_cplx[ij,:], H_complex[ij,:], V_RF_cplx[ij,:], W_RF_cplx[ij,:]])
+        bundeled_inputs_modified = [V_D_cplx, W_D_cplx, H_complex, V_RF_cplx, W_RF_cplx]
+        T0 = tf.map_fn(self.C_per_sample, bundeled_inputs_modified, fn_output_signature=tf.float32, parallel_iterations=self.BATCHSIZE) #
+        return tf.multiply(-1.0, tf.reduce_mean(T0))
 
-        return -1.0 * T / self.BATCHSIZE
+
+        # # iml 2
+        # V_D_cplx, W_D_cplx, H, V_RF_cplx, W_RF_cplx = bundeled_inputs
+        # H_complex = tf.complex(H[:, :, :, :, 0], H[:, :, :, :, 1])
+        # T = 0
+        # for ij in range(self.BATCHSIZE):
+        #     T = T + self.C_per_sample( [V_D_cplx[ij,:], W_D_cplx[ij,:], H_complex[ij,:], V_RF_cplx[ij,:], W_RF_cplx[ij,:]])
+        #
+        # return -1.0 * T / self.BATCHSIZE
 
 
     #     # impl3
