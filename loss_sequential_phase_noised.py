57d65ff5f691a2e8e25004ce3f7329d4d21c81e5
fara <fara@faraland.com>
2021-07-28 11:12:51 -0700

    sd

--- loss_sequential_phase_noised.py
+++ loss_sequential_phase_noised.py
@@ -77,14 +77,10 @@ class sequential_loss_phase_noised_class:
         # for k in range(int(self.K * self.truncation_ratio_keep)):
         #     H_tilde_k = H_tilde_k + self.H_tilde_k_calculation([H_masked[k,:], Lambda_B_masked[k,:], Lambda_U_masked[k,:]])
 
-        # todo- speed comment: Changing python slicing to tf.slicc made it ---- times faster
+        # todo- speed comment: Changing python slicing to tf.slice made it ---- times faster
         H_tilde_k = 0
         for k in range(int(self.K * self.truncation_ratio_keep)):
-            H_tilde_k = H_tilde_k + self.H_tilde_k_calculation(
-                [tf.slice(H_masked, [k, 0, 0], [1, self.N_u_a, self.N_b_a]),
-                 tf.slice(Lambda_B_masked, [k, 0, 0], [1, self.N_b_a, self.N_b_a]),
-                 tf.slice(Lambda_U_masked, [k, 0, 0], [1, self.N_u_a, self.N_u_a])])
-
+            H_tilde_k = H_tilde_k + self.H_tilde_k_calculation([tf.slice(H_masked, [k, 0, 0], [1, self.N_u_a, self.N_b_a]), tf.slice(Lambda_B_masked, [k, 0, 0], [1, self.N_b_a, self.N_b_a]), tf.slice(Lambda_U_masked, [k, 0, 0], [1, self.N_u_a, self.N_u_a])])
 
         T1 = tf.linalg.matmul(T0, H_tilde_k)
         T2 = tf.linalg.matmul(T1, V_RF)
